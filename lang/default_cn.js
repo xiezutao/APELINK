f72f1ccdd4b4f88cf4a39d51c17f4a07b5c1e99c
xietao <xchlkwj@xchlwkj.com>
2018-04-27 15:24:28 +0800

    apellink_V1.0

---
+++ lang/default_cn.js
@@ -0,0 +1,13 @@
+//js内部调用
+Lang.prototype.pack.cn = {
+    "token": {
+        "clear_all_queue_number" : "这里存放的是中文的翻译",
+        "popup_all_queuing_revocation" : "左边的是对应的class类名",
+        "aready_clear" : "这里后面不能加符号，插入请在上面插入"
+    },
+    "regex": [
+        ["Budget", "234324"],
+        ["^Something at start of text", ""],
+        ["This will be case insensitive", "i", ""]
+    ]
+};
